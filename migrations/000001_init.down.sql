852104e1ff0026b721edb4e6f831a030685e24ef
Nursultan-Salikhov <84440114+Nursultan-Salikhov@users.noreply.github.com>
2022-11-24 13:54:15 +0300

    Migrations files.

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE schedules;
+
+DROP TABLE clients_appointments;
+
+DROP TABLE appointments;
+
+DROP TABLE clients;
+
+DROP TABLE users;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users
+(
+    id serial not null unique,
+    name varchar(255) not null,
+    username varchar(255) not null unique,
+    password_hash varchar(255) not null
+);
+
+CREATE TABLE schedules
+(
+    id serial not null unique,
+    user_id int references users (id) on delete cascade not null,
+    work_day date not null,
+    start_time time not null,
+    end_time time not null
+);
+
+CREATE TABLE clients
+(
+    id serial not null unique,
+    user_id int references users (id) on delete cascade not null,
+    name varchar(255) not null,
+    phone_number varchar(20),
+    email varchar(255),
+    tg_username varchar(255),
+    description varchar(255)
+);
+
+CREATE TABLE appointments
+(
+    id serial not null unique,
+    app_day date not null,
+    app_time time not null
+);
+
+CREATE TABLE clients_appointments
+(
+    id serial not null unique,
+    client_id int references clients (id) on delete cascade not null,
+    app_id int references appointments (id) on delete cascade not null
+);
